5087a3508a626dea731a4d1a3da430477cd2eaee
RuhilMU <ruhilmuhammadukasyah2005@mail.ugm.ac.id>
2024-10-08 14:22:26 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>@section('title', 'Tim M.U')</title>
+    
+</head>
+<body>
+    <table class="table table-stripped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama Pemain</th>
+                <th>No Punggung</th>
+                <th>Posisi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data_pemain as $index => $pemain)
+                <tr>
+                    <td>{{ $index+1 }}</td>
+                    <td>{{ $pemain->nama_pemain }}</td>
+                    <td>{{ $pemain->no_punggung }}</td>
+                    <td>{{ $pemain->posisi }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <h1>Manchester United</h1>
+    </header>
+    <footer>
+        <p>© 2024 Glory Glory.</p>
+    </footer>
+</body>
+</html>
